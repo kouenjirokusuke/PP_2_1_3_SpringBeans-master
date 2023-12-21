3462f0dfc1416ad482ebcc9378794d3d44f7f93a
kouenjirokusuke <kouenjirokusuke@gmail.com>
2023-12-21 08:39:49 +0300

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,32 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("cat")
+    private Animal animal;
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal2;
+
+    @Autowired
+    private Timer timer;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(getTimer().getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
